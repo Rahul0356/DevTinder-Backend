eb485c1cf62fa143d84fc52befd03e72dd7c7089
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-25 15:59:09 +0530

    implement the user scheme

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require("express")
 
 const app = express();
 
+const { adminAuth }= require("./middlewares/auth.js")
 
-app.use("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params)
-    res.send("FirstName:Rahul, LastName:Kumar!");
+app.use ("/admin",adminAuth)
+
+app.get("/user",(req,res)=>{
+    res.send("User Data Sent")
+})
+
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data sent");
 });
 
-
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deteled a user")
+})
 app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777....");
 })
