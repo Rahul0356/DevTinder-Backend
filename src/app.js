ffd4bc35c258acbca03173775dbcd0c3e655b77d
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-27 11:55:56 +0530

    minor bug fix

--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
-const express = require("express")
-const connectDB = require("./config/database.js")
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-const User = require("../models/user.js")
-const {} = require("../utils/validation.js")
 const cookieParser = require("cookie-parser");
-const jwt = require (j)
+const cors = require("cors");
+const http = require("http");
 
-const { adminAuth }= require("./middlewares/auth.js")
+require("dotenv").config();
 
-app.use ("/admin",adminAuth)
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
+
+
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+
+const server = http.createServer(app);
 
 
 connectDB()
